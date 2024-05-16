10460379c67f7f0dd9cf25d22174d0003273c2d8
RachelKm <rachelikmy@gmail.com>
2024-05-16 15:27:59 +0300

    s

---
+++ src/Lending/tendingProtocols.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20; 
+
+contract LendingProtocol is ERC20{
+
+     address owner;
+     mapping (address -> uint) public lenders;
+     ERC20 DAI;
+     ERC20 bond;
+     uint maxLTV = 80;
+     uint collateralValue = 0;
+
+    constructor(address tokenAddress) ERC20("bond", "BND"){
+        DAI = ERC20(tokenAddress);
+    }
+
+    modifier validAmount(uint amount) {
+     require(amount > 0, "unvalid amount");
+     _;
+    }
+    function lendingDAI(uint amount) public validAmount(amount) {
+        DAI.transferFrom(msg.sender, address(this), amount);
+        lenders[msg.sender] = amount;
+        collateralValue += amount;
+        //calculate the amounts of bonds he reserve
+        _mint(msg.sender, amount);
+        
+    
+    }
+
+    function receiveLendingDAI(uint amount) external validAmount(amount) {
+       require(lenders[msg.sender] >= amount, "you did not lend enough DAI");
+       require(bond.balanceOf(msg.sender) >= amount, "you do not have enough bond tokens");
+       _burn(msg.sender, amount);
+       DAI.transfer(msg.sender, amount);
+       leders[msg.sender] -= amount;
+    
+    }
+    function borrow(uint borrowedValue) public payable validAmount(borrowedValue) {
+        uint borrowLimit = percentage((collateralValue - borrowedValue), maxLTV);
+
+    }
+
+     
+}
---
+++ src/Trading/trade1.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.20;
+
+contract Trad {
+    address owner;
+    bool oneTime = false;
+    IERC20 public 
+    constructor (){
+        owner = msg.sender;
+    }
+    function  initialize() public{
+require(msg.sender == owner,"only owner ");
+require(oneTime == false ,"one time only");
+oneTime = true;
+    }
+    function  price() public{
+
+    }
+    function  tradeAToB() public{
+
+    }
+    function  tradeBToA() public{
+ 
+    }
+      function  addLiquidity() public{
+ 
+    }
+      function  removeLiquidity() public{
+ 
+    }
+    
+
+}
